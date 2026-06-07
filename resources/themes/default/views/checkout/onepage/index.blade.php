a99ac01b034c2dd5d47cfb88c6aa4cefe033efa9
Manuel <manuel@lagmedicalinc.com>
2026-06-07 13:43:15 -0400

    Ajustes

--- resources/themes/default/views/checkout/onepage/index.blade.php
+++ resources/themes/default/views/checkout/onepage/index.blade.php
@@ -76,6 +76,7 @@
                 <div class="grid grid-cols-[1fr_auto] gap-8 max-lg:grid-cols-[1fr] max-md:gap-5">
                     <!-- Included Checkout Summary Blade File For Mobile view -->
                     <div class="hidden max-md:block">
+                        isTest
                         @include('shop::checkout.onepage.summary')
                     </div>
 
